7e7a098488b0d5ca8d5908482db5ea0302be72d7
Remics89 <Remics89@gmail.com>
2024-05-17 03:31:30 -0700

    feat: now SSR, pass props to client component, grab photo paths

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,39 +1,20 @@
-"use client";
-import { CldImage } from "next-cloudinary";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient;
+import { PhotoLibrary } from "../components/generatePhotoLib";
 
-export default function Page() {
+
+export default async function Page() {
+
+  
+  const data = await prisma.photos.findMany({
+      where: {owner: 1}
+    })
+
   return (
     <div>
-
-      <div className="md:flex flex-wrap flex-row justify-evenly">
-        <CldImage
-          className="p-5"
-          width={600}
-          height={450}
-          src="docs/models-AA"
-          sizes="100vw"
-          alt="Auto Assault"
-          title="Auto Assault"
-        />
-        <CldImage
-          className="p-5"
-          width={600}
-          height={450}
-          src="docs/models-57"
-          sizes="100vw"
-          alt="Hunt: Showdown"
-          title="Hunt: Showdown"
-        />
-        <CldImage
-          className="p-5"
-          width={600}
-          height={450}
-          src="docs/models-22"
-          sizes="100vw"
-          alt="No Man's Sky"
-          title="No Man's Sky"
-        />
-      </div>
+      <PhotoLibrary data={data} />
     </div>
   );
+
+  
 }

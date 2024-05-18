4c594f4fd1df108d015c4899efda81524e6229cb
Remics89 <Remics89@gmail.com>
2024-05-18 02:53:49 -0700

    feat: pass new props for page count and show count, changed page highlight color

--- src/app/components/pagination.js
+++ src/app/components/pagination.js
@@ -2,7 +2,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 export default function PhotoPagination(props) {
-  const { displayCount } = props;
+  const { displayCount, displayBlock, imgCount } = props;
   return (
     <div className="flex border-t items-center justify-between border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
@@ -23,8 +23,8 @@ export default function PhotoPagination(props) {
         <div>
           <p className="text-sm text-gray-700">
             Showing <span className="font-medium">1</span> to{" "}
-            <span className="font-medium">10</span> of{" "}
-            <span className="font-medium">97</span> results
+            <span className="font-medium">{displayCount}</span> of{" "}
+            <span className="font-medium">{imgCount}</span> results
           </p>
         </div>
         <div>
@@ -43,7 +43,7 @@ export default function PhotoPagination(props) {
             <a
               href="#"
               aria-current="page"
-              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="relative z-10 inline-flex items-center bg-emerald-700 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               1
             </a>

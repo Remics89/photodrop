03f77c1d7c7065fb7f51bcb3d6fa6abc761b894e
Remics89 <Remics89@gmail.com>
2024-05-19 23:07:24 -0700

    feat: mobile pagination

--- src/app/components/pagination.js
+++ src/app/components/pagination.js
@@ -2,22 +2,68 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 export default function PhotoPagination(props) {
-  const { displayCount, displayBlock, imgCount } = props;
+  const { displayCount, imgCount, selectedPage, setSelectedPage, numOfPages } = props;
+
+  const activePageColor = "bg-emerald-700";
+  const pageNumberClassNames = [
+    {
+      className:
+        "relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold text-white",
+    },
+    {
+      className:
+        "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50",
+    },
+  ];
+
+  const pageHandler = (e, num) => {
+    e.preventDefault();
+    if (num === "-") {
+      setSelectedPage(selectedPage - 1);
+    } else if (num === "+") {
+      setSelectedPage(selectedPage + 1);
+    } else {
+      setSelectedPage(num);
+    }
+  };
+
   return (
     <div className="flex border-t items-center justify-between border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Previous
-        </a>
-        <a
-          href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Next
-        </a>
+        {selectedPage == 1 ? (
+          <button
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-gray-600 px-4 py-2 text-sm font-medium text-white opacity-20"
+            disabled
+          >
+            Previous
+          </button>
+        ) : (
+          <button
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            onClick={(e) => {
+              pageHandler(e, "-");
+            }}
+          >
+            Previous
+          </button>
+        )}
+        {numOfPages[0] == selectedPage ? (
+          <button
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-gray-600 px-4 py-2 text-sm font-medium text-white opacity-20"
+            disabled
+          >
+            Next
+          </button>
+        ) : (
+          <button
+            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            onClick={(e) => {
+              pageHandler(e, "+");
+            }}
+          >
+            Next
+          </button>
+        )}
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
@@ -40,17 +86,21 @@ export default function PhotoPagination(props) {
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-emerald-700 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
+            {numOfPages.map((num, index) => {
+              return (
+                <a
+                  key={index}
+                  aria-current="page"
+                  className={`${pageNumberClassNames[0].className} ${activePageColor}`}
+                  onClick={(e) => {
+                    pageHandler(e, num);
+                  }}
+                >
+                  1
+                </a>
+              );
+            })}
+            <a href="#" className={`${pageNumberClassNames[1].className}`}>
               2
             </a>
             <a

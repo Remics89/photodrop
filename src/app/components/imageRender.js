c0a08007d21f6cd3998c990cecda727a7001eb94
Remics89 <Remics89@gmail.com>
2024-08-14 01:53:17 -0700

    moved image cdn call to own file

---
+++ src/app/components/imageRender.js
@@ -0,0 +1,24 @@
+"use client";
+import { CldImage } from "next-cloudinary";
+
+export default function RenderImages (props) {
+  const { images } = props;
+  return (
+    <div className="md:flex flex-wrap flex-row justify-evenly px-5">
+      {images.map((photoData, index) => {
+        return (
+          <div key={index}>
+            <CldImage
+              className="m-2 shadow-sm shadow-black hover:shadow-md hover:shadow-black mobile:my-2 mobile:mx-0 mobile:w-100%"
+              width={600}
+              height={450}
+              src={photoData.path}
+              alt={photoData.title || "alt placeholder"} 
+              title={photoData.title}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};

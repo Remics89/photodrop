7556928c485bac93b496758ae69ebd6ec743728e
Remics89 <Remics89@gmail.com>
2024-08-14 01:55:09 -0700

    simplified page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,17 +1,17 @@
-
 import prisma from "../../../prisma/prisma";
-import { PhotoLibrary } from "../components/generatePhotoLib";
+import { PhotoLibrary } from "../components/PhotoLib";
+import { NoImages } from "../components/noImages";
 
 export default async function Page() {
-  const data = await prisma.photos.findMany({
+  const images = await prisma.photos.findMany({
     where: { owner: "testuser" },
   });
 
-  const imgCount = data.length;
-
   return (
     <div>
-      <PhotoLibrary data={data} imgCount={imgCount} />
+      {images.length > 0 ? <PhotoLibrary
+        data={images}
+      /> : <NoImages/> }
     </div>
   );
 }

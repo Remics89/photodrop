437a5aa2547f7e47fcbdc9a7b250a0c5ce60e747
Remics89 <Remics89@gmail.com>
2024-08-14 01:53:35 -0700

    no images component

---
+++ src/app/components/noImages.js
@@ -0,0 +1,9 @@
+export const NoImages = () => {
+  return (
+    <div className="flex justify-center">
+      <p className="flex justify-center text-sm leading-6 text-white bg-gray-800 w-3/6 rounded-xl my-5 py-2.5">
+        <strong className="font-semibold">There are no images to show.</strong>
+      </p>
+    </div>
+  );
+};

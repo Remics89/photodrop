f677d445959b28436f72284f30b73af633ecb64e
Remics89 <Remics89@gmail.com>
2024-08-23 02:24:58 -0700

    added temporary nav option

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,7 +3,9 @@ import { Fragment, useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
+// **** Dashboard is temporary. Remove before production
 const navigation = [
+  { name: "Dashboard", href: "/dashboard" },
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
 ];
@@ -24,8 +26,6 @@ export default function Header() {
   const [auth, setAuth] = useState(false);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-
-
   return (
     <header className="bg-gray-900">
       <nav
@@ -33,7 +33,7 @@ export default function Header() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="/dashboard" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
@@ -57,7 +57,7 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
@@ -77,7 +77,8 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/25">
               <div className="space-y-2 py-6">
-                {!auth ? navigation.map((item) => (
+                {!auth
+                  ? navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
@@ -85,7 +86,8 @@ export default function Header() {
                       >
                         {item.name}
                       </a>
-                    )) : userNavigation.map((item) => (
+                    ))
+                  : userNavigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
@@ -93,24 +95,24 @@ export default function Header() {
                       >
                         {item.name}
                       </a>
-                      ))}
+                    ))}
               </div>
-                  <div className="py-6">
-                    <a
-                      href="/login"
-                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                  <div className="py-6">
-                    <a
-                      href="/register"
-                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                    >
-                      Sign Up
-                    </a>
-                  </div>
+              <div className="py-6">
+                <a
+                  href="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                >
+                  Log in
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="/register"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           </div>
         </Dialog.Panel>

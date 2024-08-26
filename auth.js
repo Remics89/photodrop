7fb48f83a77557b591aa6d51f4ae991bae919892
Remics89 <Remics89@gmail.com>
2024-08-26 01:44:28 -0700

    added kv storage controller

--- auth.js
+++ auth.js
@@ -6,5 +6,14 @@ import { NextAuthConfig } from "next-auth";
 import { UnstorageAdapter } from "@auth/unstorage-adapter";
 
 import Google from "next-auth/providers/google";
+import { createStorage } from "unstorage";
 
-
+const storage = createStorage({
+    driver: process.env.VERCEL ? 
+        vercelKVDriver({
+            url: process.env.KV_REST_API_URL,
+            token: process.env.KV_REST_API_TOKEN,
+            env: false
+        })
+        : memoryDriver()
+})
